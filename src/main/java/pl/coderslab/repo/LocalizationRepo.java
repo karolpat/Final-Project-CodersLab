7b62c0792c46324956068d6ae5691be563573b7d
Karol Patecki <karol.patecki09@gmail.com>
2018-02-04 15:42:44 +0100

    Show user details

---
+++ src/main/java/pl/coderslab/repo/LocalizationRepo.java
@@ -0,0 +1,11 @@
+package pl.coderslab.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.coderslab.entity.Localization;
+
+public interface LocalizationRepo extends JpaRepository<Localization, Long> {
+
+	
+	Localization findByUserId(long id);
+}
--- src/main/java/pl/coderslab/web/HomeController.java
+++ src/main/java/pl/coderslab/web/HomeController.java
@@ -2,11 +2,14 @@ package pl.coderslab.web;
 
 import javax.validation.Valid;
 
+import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.SortDefault;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -18,9 +21,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import pl.coderslab.entity.Faq;
-import pl.coderslab.entity.Role;
+import pl.coderslab.entity.Localization;
 import pl.coderslab.entity.User;
 import pl.coderslab.repo.FaqRepo;
+import pl.coderslab.repo.LocalizationRepo;
 import pl.coderslab.repo.RoleRepo;
 import pl.coderslab.repo.StudentRepo;
 import pl.coderslab.service.UserService;
@@ -31,39 +35,75 @@ public class HomeController {
 	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
 	private UserService userService;
-	
+
 	@Autowired
 	private StudentRepo stud;
-	
+
 	@Autowired
 	private FaqRepo faqRepo;
-	
+
 	@Autowired
 	private RoleRepo roleRepo;
 	
+	@Autowired
+	private LocalizationRepo localRepo;
+
 	public HomeController(UserService userService) {
-		this.userService=userService;
+		this.userService = userService;
+	}
+
+	public String currentUser() {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
 	}
 	
+	@RequestMapping({"/user/profile", "/manager/profile"})
+	public String local(Model model) {
+		
+		User user = userService.findByUserName(currentUser());
+		Localization local = localRepo.findByUserId(user.getId());
+		
+		model.addAttribute("local", local);
+		model.addAttribute("currUser", user);
+		return "profile";
+	}
+	
+	@RequestMapping("/charts")
+	public String charts() {
+		return "charts";
+	}
+	
+	@RequestMapping("/forms")
+	public String forms() {
+		return "forms";
+	}
+	
+	@RequestMapping("/tables")
+	public String tables() {
+		return "tables";
+	}
+
 	@GetMapping("/register")
 	public String registerForm(Model model) {
-		
+
 		User u = new User();
 		model.addAttribute("user", u);
-		
+
 		model.addAttribute("roleList", roleRepo.findAll());
 		return "register";
 	}
-	
-	@RequestMapping(value="/register", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String register(@Valid User user, BindingResult bresult, Model model) {
 		if (bresult.hasErrors()) {
-			log.info("błąd"+user.toString());
+			log.info("błąd" + user.toString());
 			model.addAttribute(user);
 			return "/register";
 
 		} else {
 			log.info(user.toString());
+			user.setCreated(new LocalDate());
 			userService.saveUser(user);
 			return "redirect:/login";
 		}
@@ -78,31 +118,24 @@ public class HomeController {
 	@GetMapping("/")
 	public String index(Model model) {
 		log.info("some log");
-		model.addAttribute("faqList",faqRepo.findAll());
+		model.addAttribute("faqList", faqRepo.findAll());
 		return "index";
 	}
-	
+
 	@RequestMapping("/like")
 	public String indexFaq(@RequestParam long id) {
 		Faq faq = faqRepo.findOne(id);
-		faq.setRate(faq.getRate()+1);
+		faq.setRate(faq.getRate() + 1);
 		faqRepo.save(faq);
-		
+
 		return "redirect:/";
 	}
 
-	
-	@RequestMapping("/forms")
-	public String forms() {
-		return "forms";
-	}
-	
-	
-	
+
 	@RequestMapping("/students")
-	public String list(ModelMap model, @SortDefault("firstName") Pageable pageable){
+	public String list(ModelMap model, @SortDefault("firstName") Pageable pageable) {
 		model.addAttribute("studs", stud.findAll(pageable));
-		
+
 		return "list";
 	}
 }
