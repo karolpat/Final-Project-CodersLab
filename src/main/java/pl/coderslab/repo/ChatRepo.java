e436100b97cc0c75966542ae58e2fa04b10f5def
karolpat <karol.patecki09@gmail.com>
2018-02-13 23:01:28 +0100

    Possibility to search messeges and chats

--- src/main/java/pl/coderslab/repo/ChatRepo.java
+++ src/main/java/pl/coderslab/repo/ChatRepo.java
@@ -1,11 +1,18 @@
 package pl.coderslab.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.coderslab.entity.Chat;
+import pl.coderslab.entity.Message;
 
 public interface ChatRepo extends JpaRepository<Chat, Long> {
 
 	
 	Chat findByMessageSendToIdAndMessageSendFromId(long idTo, long idFrom);
+	
+	List<Chat> findAllByUserId(long id);
+	
+	
 }
--- src/main/java/pl/coderslab/repo/MessageRepo.java
+++ src/main/java/pl/coderslab/repo/MessageRepo.java
@@ -1,9 +1,12 @@
 package pl.coderslab.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.coderslab.entity.Message;
 
 public interface MessageRepo extends JpaRepository<Message, Long> {
 
+	List<Message> findAllByChatId(long id);
 }
